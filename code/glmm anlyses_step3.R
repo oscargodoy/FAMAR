773e00fe7df9525a7503eb824076ff9d2b20c2d8
Oscar Godoy <ogodoy.re@gmail.com>
2018-10-09 09:29:16 +0200

    some analyses and data

---
+++ code/glmm anlyses_step3.R
@@ -0,0 +1,32 @@
+#load the data and start with the analyses
+library(reshape2)
+library(nlme)
+
+data <- read.csv("data/famar_clean.csv", header=T, sep=",")
+#We are going to establish a linear relationship between biomass
+#production and the predictors by doing a log of biomass
+lCtr <- lmeControl(maxIter = 500, msMaxIter = 500, tolerance = 1e-6, niterEM = 250, msMaxEval = 200)
+
+#1.leaf dry weight (L.DW)----
+model_ldw1 <- lme(log(L.DW) ~ SITE, data=data, random=~1|REPLICATE, control=lCtr, correlation= corAR1(),
+                method='REML',na.action=na.omit)
+
+model_ldw2 <- lme(log(L.DW) ~ SITE + NH4.uM.mean, data=data, random=~1|REPLICATE, control=lCtr, correlation= corAR1(),
+                  method='REML',na.action=na.omit)
+
+model_ldw3 <- lme(log(L.DW) ~ SITE + NH4.uM.mean + NO3.uM.mean, data=data, random=~1|REPLICATE, control=lCtr, correlation= corAR1(),
+                  method='REML',na.action=na.omit)
+
+AIC(model_ldw1, model_ldw2, model_ldw3)
+#Model checking plot 
+residuals <- resid(model_ldw3)
+plot(fitted(model_ldw3), residuals)
+abline(0,0)
+plot(fitted(model_ldw3), log(data$L.DW)) # this lines does not work yet
+qqnorm(residuals)
+qqline(residuals)
+
+#2.below ground dry weight (BG.DW)----
+
+#model1 <- lme(log(L.DW) ~ SITE, data=data, random=~1|REPLICATE, control=lCtr, correlation=corAR1(form=~S.date), method='REML', na.action=na.omit)
+
--- code/temporal series_step 2.R
+++ code/temporal series_step 2.R
@@ -8,6 +8,8 @@ data <- subset(data, data$YEAR<2015)
 table(data$YEAR)
 table(data$SITE)
 
+write.csv(data, file="data/famar_clean.csv")
+
 #order by season than make biological sense
 print(levels(data$SEASON))
 data$SEASON <- factor(data$SEASON, levels(data$SEASON)[c(4,2,3,1)])
@@ -19,23 +21,126 @@ data_cn3 <- subset(data, data$SITE=="CN3")
 #Can I use replicates to analyze partial correlation in temporal series?
 #If not, average to have a single value per time.
 library(plyr)
+
+#L.DW---- 
 data_cn1_ave <- ddply(data_cn1, 
                        c("S.date"),
                       summarise,
                       L.DW=quantile(L.DW, c(0.5), na.rm=T, type=8))
 acfRes_cn1 <- acf(data_cn1_ave$L.DW)
+pacfRes_cn1 <- pacf(data_cn1_ave$L.DW) 
 
 data_cn2_ave <- ddply(data_cn2, 
                       c("S.date"),
                       summarise,
                       L.DW=quantile(L.DW, c(0.5), na.rm=T, type=8))
 acfRes_cn2 <- acf(data_cn2_ave$L.DW)
+pacfRes_cn2 <- pacf(data_cn2_ave$L.DW) 
 
 data_cn3_ave <- ddply(data_cn3, 
                       c("S.date"),
                       summarise,
                       L.DW=quantile(L.DW, c(0.5), na.rm=T, type=8))
 acfRes_cn3 <- acf(data_cn3_ave$L.DW)
+pacfRes_cn3 <- pacf(data_cn3_ave$L.DW) 
 
-#most likely we need an autocorrelation structure of COR AR 2
+#most likely we need an autocorrelation structure of COR AR 2 with L.DW
 
+#BG.DW----
+data_cn1_ave <- ddply(data_cn1, 
+                      c("S.date"),
+                      summarise,
+                      BG.DW=quantile(BG.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn1 <- acf(data_cn1_ave$BG.DW)
+pacfRes_cn1 <- pacf(data_cn1_ave$BG.DW) 
+
+data_cn2_ave <- ddply(data_cn2, 
+                      c("S.date"),
+                      summarise,
+                      BG.DW=quantile(BG.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn2 <- acf(data_cn2_ave$BG.DW)
+pacfRes_cn2 <- pacf(data_cn2_ave$BG.DW) 
+
+data_cn3_ave <- ddply(data_cn3, 
+                      c("S.date"),
+                      summarise,
+                      BG.DW=quantile(BG.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn3 <- acf(data_cn3_ave$BG.DW)
+pacfRes_cn3 <- pacf(data_cn3_ave$BG.DW)
+
+#Partial autocorrelogram suggests that either no auto temporal cor structure of if so
+#put the one of the previous time. 
+
+#Ri.DW----
+data_cn1_ave <- ddply(data_cn1, 
+                      c("S.date"),
+                      summarise,
+                      Ri.DW=quantile(Ri.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn1 <- acf(data_cn1_ave$Ri.DW)
+pacfRes_cn1 <- pacf(data_cn1_ave$Ri.DW) 
+
+data_cn2_ave <- ddply(data_cn2, 
+                      c("S.date"),
+                      summarise,
+                      Ri.DW=quantile(Ri.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn2 <- acf(data_cn2_ave$Ri.DW)
+pacfRes_cn2 <- pacf(data_cn2_ave$Ri.DW) 
+
+data_cn3_ave <- ddply(data_cn3, 
+                      c("S.date"),
+                      summarise,
+                      Ri.DW=quantile(Ri.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn3 <- acf(data_cn3_ave$Ri.DW)
+pacfRes_cn3 <- pacf(data_cn3_ave$Ri.DW)
+
+#Partial autocorrelogram suggests that either no auto temporal cor structure of if so
+#put the one of the previous time. 
+
+#Ro.DW----
+data_cn1_ave <- ddply(data_cn1, 
+                      c("S.date"),
+                      summarise,
+                      Ro.DW=quantile(Ro.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn1 <- acf(data_cn1_ave$Ro.DW)
+pacfRes_cn1 <- pacf(data_cn1_ave$Ro.DW) 
+
+data_cn2_ave <- ddply(data_cn2, 
+                      c("S.date"),
+                      summarise,
+                      Ro.DW=quantile(Ro.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn2 <- acf(data_cn2_ave$Ro.DW)
+pacfRes_cn2 <- pacf(data_cn2_ave$Ro.DW) 
+
+data_cn3_ave <- ddply(data_cn3, 
+                      c("S.date"),
+                      summarise,
+                      Ro.DW=quantile(Ro.DW, c(0.5), na.rm=T, type=8))
+acfRes_cn3 <- acf(data_cn3_ave$Ro.DW)
+pacfRes_cn3 <- pacf(data_cn3_ave$Ro.DW)
+#Partial autocorrelogram suggests that either no auto temporal cor structure of if so
+#put the one of the previous time. 
+
+#dens.TOT----
+
+data_cn1_ave <- ddply(data_cn1, 
+                      c("S.date"),
+                      summarise,
+                      dens.TOT=quantile(dens.TOT, c(0.5), na.rm=T, type=8))
+acfRes_cn1 <- acf(data_cn1_ave$dens.TOT)
+pacfRes_cn1 <- pacf(data_cn1_ave$dens.TOT) 
+
+data_cn2_ave <- ddply(data_cn2, 
+                      c("S.date"),
+                      summarise,
+                      dens.TOT=quantile(dens.TOT, c(0.5), na.rm=T, type=8))
+acfRes_cn2 <- acf(data_cn2_ave$dens.TOT)
+pacfRes_cn2 <- pacf(data_cn2_ave$dens.TOT) 
+
+data_cn3_ave <- ddply(data_cn3, 
+                      c("S.date"),
+                      summarise,
+                      dens.TOT=quantile(dens.TOT, c(0.5), na.rm=T, type=8))
+acfRes_cn3 <- acf(data_cn3_ave$dens.TOT)
+pacfRes_cn3 <- pacf(data_cn3_ave$dens.TOT)
+#Partial autocorrelogram suggests that either no auto temporal cor structure of if so
+#put the one of the previous time or two times ago. 
