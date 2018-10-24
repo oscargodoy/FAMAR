f4b89257fd1b9cd9e9ae0cc871168a38a20e8beb
Oscar Godoy <ogodoy.re@gmail.com>
2018-10-24 23:35:16 +0200

    new analyses

--- code/glmm anlyses_step3.R
+++ code/glmm anlyses_step3.R
@@ -19,7 +19,7 @@ data$PLOT <- paste(data$SITE, data$R.replicates, sep="_")
 lCtr <- lmeControl(maxIter = 500, msMaxIter = 500, tolerance = 1e-6, niterEM = 250, msMaxEval = 200)
 
 #1.leaf dry weight (L.DW)----
-model_ldw1 <- lme(log(L.DW) ~ SITE, data=data, random=~1|PLOT, control=lCtr, correlation= corARMA(p=2, q=3),
+model_ldw1 <- lme(log(L.DW) ~ SITE, data=data, random=~1|PLOT, control=lCtr, correlation= corARMA(p=2, q=3), 
                   weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit)
 #According to AIC an autocorrelation structure with CorARMA (p=2, q=3) is the best (lowest AIC)
 #Now I am adding another potential predictors. 
@@ -128,18 +128,198 @@ qqline(residuals)
 library(reshape2)
 library(nlme)
 
-#data <- read.csv("data/famar-Biom_123_05_15_nut.c.csv", header=T, sep=",")
-data <- read.csv("data/FAMAR_punching123_05_15_nut.c.csv", header=T, sep=",")
-data <- subset(data, data$YEAR>2005)
-data <- subset(data, data$YEAR<2015)
-table(data$YEAR)
-table(data$SITE)
+bio <- read.csv("data/famar-Biom_123_05_15_minVAR.csv", header=T, sep=",")
+bio <- subset(bio, bio$YEAR>2005)
+bio <- subset(bio, bio$YEAR<2015)
+table(bio$YEAR)
+table(bio$SITE)
 #put the correct format
-data$S.date <- as.POSIXct(strptime(data$S.date, format="%d/%m/%Y")) 
+bio$S.date <- as.POSIXct(strptime(bio$S.date, format="%d/%m/%Y")) 
 
 lCtr <- lmeControl(maxIter = 500, msMaxIter = 500, tolerance = 1e-6, niterEM = 250, msMaxEval = 200)
 
-model_ldw3 <- lme(log(L.DW) ~ SITE + NH4.uM.q2 + NO3.uM.q2 + PO4.uM.q2, data=data, random=~1|PLOT, control=lCtr, correlation= corARMA(p=2, q=3),
+#5.1 LDW----
+model_ldw1 <- lme(log(L.DW) ~ ELEVATION + NH4.uM.mean + NO3.uM.mean, data=bio, random=~1|R.replicates/SITE, control=lCtr, correlation= corARMA(p=2, q=2),
                   weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
 # This last model is the one that work best 
-AIC(model_ldw1, model_ldw2, model_ldw3)
+summary(model_ldw)
+
+#Model checking plot 
+residuals <- resid(model_ldw)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_ldw)
+qqnorm(model_ldw, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#5.2 BGDW----
+model_bgdw <- lme(log(BG.DW) ~ ELEVATION + YEAR + NO3.uM.mean, data=bio, random=~1|R.replicates/SITE, control=lCtr, correlation= corAR1(),
+                   weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
+
+# This last model is the one that work best 
+summary(model_bgdw)
+
+
+#Model checking plot 
+residuals <- resid(model_bgdw)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_bgdw)
+qqnorm(model_bgdw, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#5.3 dens.TOT----
+model_densTOT <- lme(log(dens.TOT) ~ ELEVATION*NO3.uM.mean + NO2.uM.mean + NH4.uM.mean, data=bio, random=~1|R.replicates/SITE, control=lCtr, correlation= corARMA(p=2, q=2),
+                     weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
+
+# This last model is the one that work best 
+summary(model_densTOT)
+
+
+#Model checking plot 
+residuals <- resid(model_densTOT)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_densTOT)
+qqnorm(model_densTOT, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#5.4 SDW----
+model_sdw <- lme(log(SDW) ~ ELEVATION*NO3.uM.mean + NO2.uM.mean + NH4.uM.mean, data=bio, random=~1|R.replicates/SITE, control=lCtr, correlation= corAR1(),
+                  weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
+
+
+# This last model is the one that work best 
+summary(model_sdw)
+
+#Model checking plot 
+residuals <- resid(model_sdw)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_sdw)
+qqnorm(model_sdw, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#load the data and start with the analyses
+library(reshape2)
+library(nlme)
+punch <- read.csv("data/FAMAR_punching123_05_15_minVAR.csv", header=T, sep=",")
+punch <- subset(punch, punch$YEAR>2005)
+punch <- subset(punch, punch$YEAR<2015)
+table(punch$YEAR)
+table(punch$SITE)
+#put the correct format
+punch$S.date <- as.POSIXct(strptime(punch$S.date, format="%d/%m/%Y")) 
+
+#6.1 LGR----
+lCtr <- lmeControl(maxIter = 500, msMaxIter = 500, tolerance = 1e-6, niterEM = 250, msMaxEval = 200)
+
+model_lgr <- lme(log(LGR) ~ YEAR + ELEVATION.m * NO2.uM.mean, data=punch, random=~1|R.replicate/SITE, control=lCtr, correlation= corARMA(p=0, q=2),
+                 weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
+
+# This last model is the one that work best 
+summary(model_lgr)
+
+#Model checking plot 
+residuals <- resid(model_lgr)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_lgr)
+qqnorm(model_lgr, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#6.2 LLRW----
+#Log of 0 is equal to -INF so we need to put a value over zero for these cases
+punch$LLRw[punch$LLRw == 0] <- 0.01
+model_llrw <- lme(log(LLRw) ~  YEAR + ELEVATION.m * NO2.uM.mean + NO3.uM.mean + PO4.uM.mean, data=punch, random=~1|R.replicate/SITE, control=lCtr, correlation= corARMA(p=0, q=2),
+                 weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
+
+summary(model_llrw)
+
+#Model checking plot 
+residuals <- resid(model_llrw)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_llrw)
+qqnorm(model_llrw, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+#This is the model in which normality assumption are less clear. 
+
+#6.3 TurnR----
+#For this variable is already normally distributed therefore no need to log-transform it. 
+model_turnr <- lme(TurnR ~ ELEVATION.m*NH4.uM.mean + NO2.uM.mean, data=punch, random=~1|R.replicate/SITE, control=lCtr, correlation= corARMA(p=0, q=2),
+                    weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit) 
+summary(model_turnr)
+
+#Model checking plot 
+residuals <- resid(model_turnr)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_turnr)
+qqnorm(model_turnr, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#6.4 SDW----
+#For this variable is already normally distributed therefore no need to log-transform it. 
+model_sdw <- lme(log(SDW) ~ YEAR + ELEVATION.m*NO2.uM.mean, data=punch, random=~1|R.replicate/SITE, control=lCtr, correlation= corAR1(),
+                 weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit)
+
+summary(model_sdw)
+
+#Model checking plot 
+residuals <- resid(model_sdw)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_sdw)
+qqnorm(model_sdw, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
+
+#6.5 PI----
+#For this variable is already normally distributed therefore no need to log-transform it. 
+model_pi <- lme(log(PI) ~ ELEVATION.m + NO2.uM.mean + NH4.uM.mean + PO4.uM.mean, data=punch, random=~1|R.replicate/SITE, control=lCtr, correlation= corARMA(p=0, q=2),
+                 weights = varIdent(form= ~ 1 | SEASON), method='ML',na.action=na.omit)
+summary(model_pi)
+
+#Model checking plot 
+residuals <- resid(model_pi)
+hist(residuals, prob=TRUE, col="darkgray")
+Range = seq(min(residuals), max(residuals), length = length(residuals))
+Norm = dnorm(Range, mean = mean(residuals), sd = sd(residuals))
+lines(Range, Norm, col = "blue", lwd = 2)
+
+plot(model_pi)
+qqnorm(model_pi, ~ranef(., level=1))
+qqnorm(residuals)
+qqline(residuals)
